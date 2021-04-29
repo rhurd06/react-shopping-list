a32390897dc661652f55a0e371f996b97078af50
Robyn Noble <robynhurd09@gmail.com>
2021-04-29 14:26:22 -0500

    server side put route written, not tested

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -4,5 +4,25 @@ const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
 
+//PUT Route
+router.put('/:id', (req, res) => {
+    let itemId = req.params.id;
+    let boolean = req.body.boolean;
+    let sqlText = '';
+    if (boolean === 'true'){
+    let sqlText = `UPDATE "list" SET "purchased"=true WHERE "id"=$1;`;
+    } else {
+        res.sendStatus(500);
+        return;
+    }
+    pool.query(sqlText, [itemId])
+    .then((resDB) => {
+        res.sendStatus(200);
+    })
+    .catch((error) => {
+        console.log('Error with request', error);
+        res.sendStatus(500);
+    });
+});
 
 module.exports = router;
