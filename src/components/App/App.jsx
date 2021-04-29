de5e8b87f2f2a09617c9e6bc01861dbd1e54e7f9
Robyn Noble <robynhurd09@gmail.com>
2021-04-29 16:42:39 -0500

    updated app file with groceryList info

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import Header from '../Header/Header.jsx'
+import Header from '../Header/Header.jsx';
+import GroceryList from '../GroceryList/GroceryList';
+
 import './App.css';
 
 
 function App() {
+    const [groceryList, setGroceryList] = useState([]);
+
     return (
         <div className="App">
             <Header />
             <main>
-                <p>Under Construction...</p>
+                <GroceryList list={groceryList} />
             </main>
         </div>
     );
