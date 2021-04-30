3aa6f6ae24c7aff29a9333e21560f048e1b0b293
Chloe Everson <chloeliz93@gmail.com>
2021-04-30 09:24:19 -0500

    added axios post route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Header from '../Header/Header.jsx'
 import GroceryForm from '../GroceryForm/GroceryForm'
@@ -13,7 +13,33 @@ function App() {
     let [newGroceryItem, setNewGroceryItem] = useState('');
     let [newQuantity, setNewQuantity] = useState('');
 
-    const handleSubmit
+    //on load, get groceries
+    useEffect(() => {
+        getShoppingList()
+    }, [])
+
+    const handleSubmit = (event) => {
+        if (newGroceryItem) {
+            addGroceries();
+        }
+        else {
+            alert('The new grocery item needs a name');
+        }
+    }
+
+    const addGroceries = () => {
+        axios.post('/groceries', {name: newGroceryItem, quantity: newQuantity})
+            .then(response => {
+                //clear inputs
+                setNewGroceryItem('');
+                setNewQuantity('');
+                getShoppingList();
+            })
+            .catch(err => {
+                alert('error adding grocery item');
+                console.log(err);
+            })
+    };//end addGroceries
 
     return (
         <div className="App">
